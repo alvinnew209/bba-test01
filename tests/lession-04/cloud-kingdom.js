35b450a24ba0f20788450ef8af799b2de145ef31
test 01 - Long Hoang <alvinnew209@gmail.com>
2026-01-29 23:44:04 +0700

    feat: thêm cách giải 2 cho bài tập tính coin

--- tests/lession-04/cloud-kingdom.js
+++ tests/lession-04/cloud-kingdom.js
@@ -5,28 +5,29 @@ const level2Coins = 30;
 const level3Coins = 45;
 
 // Tính tổng coin
-let totalCoins = level1Coins + level2Coins + level3Coins;
+const totalCoins = level1Coins + level2Coins + level3Coins;
 console.log(totalCoins);
 
 // Tính trung bình coin
-let averageCoins = totalCoins / 3;
+const averageCoins = totalCoins / 3;
 console.log(averageCoins);
 
+// Cách 2: Dùng hàm reduce trong Array Utils mở rộng
+// Giải thích: Ban đâu em đã có ý tưởng gom vào 1 object array, nhưng mỗi item e lại viết là
+// const coinsBasedLevel = [
+// {"Level 1": 25},
+// {"Level 2": 30},
+// {"Level 3": 45}
+// ]
+// Nhưng nhận ra nó không có 1 biến chung để gán và lặp => mãi mới nghĩ ra kiểu khai báo khác ^^
+
 const coinsBasedLevel = [
-    {"Level 1": 25},
-    {"Level 2": 30},
-    {"Level 3": 45}
+    { Level: 1, coin: 25 },
+    { Level: 2, coin: 30 },
+    { Level: 3, coin: 45 },
 ]
-coinsBasedLevel.forEach(function(value) {
+const totalCoins2 = coinsBasedLevel.reduce((total, level) => total + level.coin, 0);
+console.log(totalCoins2);
 
-console.log(value);
-
-});
-
-const numbers = [1, 2, 3, 4, 5];
-
-numbers.forEach(function(value) {
-
-console.log(value);
-
-});
+const averageCoins2 = totalCoins2 / coinsBasedLevel.length;
+console.log(averageCoins2);
