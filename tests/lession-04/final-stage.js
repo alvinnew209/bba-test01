fab2153cd6739a3d17dd85ffbe67176692dfa27e
test 01 - Long Hoang <alvinnew209@gmail.com>
2026-01-30 22:46:50 +0700

    tạm done bài test

---
+++ tests/lession-04/final-stage.js
@@ -0,0 +1,12 @@
+function findPairDivisibleBy17() {
+    for (let i = 1, i <= 100, i++) {
+        console.log(i);
+        for (let j = 1, j <= 100, j++) {
+            const 
+
+        }
+    }
+    const count =
+    console.log(`\nTổng cộng: ${count} cặp`);
+    )
+}
